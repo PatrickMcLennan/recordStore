b45f2f5bc0bf9525f168ea7c8b174620c6481123
Patrick <patrick.a.mclennan@gmail.com>
2019-02-27 22:46:48 -0500

    first commit

--- client/pages/account.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const Account = ({ user }) => (
-  <div>
-    <p>{user ? 'Hello' : 'No user on account page'}</p>
-  </div>
-);
-
-export default Account;
--- client/pages/records.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const Records = ({ user }) => (
-  <div>
-    <p>{user ? 'Something is wrong' : 'No user on records page'}</p>
-  </div>
-);
-
-export default Records;
