345d563d165ef5fc4355f3c2d4b982346dde2834
Patrick <patrick.a.mclennan@gmail.com>
2019-03-22 20:59:05 -0400

    final touch ups

--- client/components/styles/form.styles.js
+++ client/components/styles/form.styles.js
@@ -39,7 +39,7 @@ const Form = styled.form`
       color: ${({ theme: { colors } }) => colors.whitePrimary};
       font-style: normal;
       cursor: pointer;
-      width: 60%;
+      width: 65%;
 
       p {
         text-align: right;
